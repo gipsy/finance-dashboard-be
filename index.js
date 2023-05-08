4b261933e47dfc342a395c7e838b3c3bf8d03d4a
gipsy <ter4444@gmail.com>
2023-05-08 15:06:19 +0300

    added Product, Transaction models and DB seed for it

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import kpiRoutes from './routes/kpi.js'
+import productRoutes from './routes/product.js'
 import KPI from './models/KPI.js'
-import { kpis } from './data/data.js'
+import Product from './models/Product.js'
+import { kpis, products } from './data/data.js'
 
 /*CONFIGURATIONS*/
 dotenv.config()
@@ -21,11 +23,14 @@ app.use(cors())
 
 /*ROUTES*/
 app.use("/kpi", kpiRoutes)
+app.use("/product", productRoutes)
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 9000
 mongoose.connect(process.env.MONGO_URL).then(async () => {
   app.listen(PORT, () => console.log(`Server Port ${PORT}`))
-  await mongoose.connection.db.dropDatabase()
-  await KPI.insertMany(kpis)
+  /*ADD DATA ONCE*/
+  // await mongoose.connection.db.dropDatabase()
+  // await KPI.insertMany(kpis)
+  // await Product.insertMany(products)
 }).catch((error) => console.log(`${error} did not connect`))
--- models/KPI.js
+++ models/KPI.js
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
 import { loadType } from 'mongoose-currency'
 
-const schema = mongoose.Schema
 loadType(mongoose)
 
 const daySchema = new Schema(
